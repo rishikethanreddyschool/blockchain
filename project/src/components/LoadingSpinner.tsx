115f5dacdd9cbfd90d1164c09f70826c69755104
Rishi Kethan Reddy <palarishikethanreddy@gmail.com>
2025-11-09 11:25:20 +0530

    Added .gitignore

--- project/src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LoadingSpinner() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
-    </div>
-  );
-}
